81291655598238823ad24be84f408a94f36b9a26
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-04-22 19:56:13 -0300

    4 commit tp2

--- practicos/63717 - González, Octavio/tp2/Compilador.cs
+++ practicos/63717 - González, Octavio/tp2/Compilador.cs
@@ -29,4 +29,50 @@ namespace Calculadora
             Valor = valor;
         }
     }
+
+    // Esta clase es la que lee la formula y la separa en partes (tokens)
+    public class Lexer
+    {
+        private string _texto;
+        private int _posicion;
+
+        public Lexer(string texto)
+        {
+            _texto = texto;
+            _posicion = 0;
+        }
+
+        public List<Token> Escanear()
+        {
+            List<Token> tokens = new List<Token>();
+
+            while (_posicion < _texto.Length)
+            {
+                char actual = _texto[_posicion];
+
+                // Si es un espacio, lo saltamos y seguimos
+                if (char.IsWhiteSpace(actual))
+                {
+                    _posicion++;
+                    continue;
+                }
+
+                // Si es una x (o X), guardamos el token de variable
+                if (actual == 'x' || actual == 'X')
+                {
+                    tokens.Add(new Token(TipoToken.VariableX, "x"));
+                    _posicion++;
+                    continue;
+                }
+
+                
+                
+                throw new Exception("No entiendo que es este simbolo: " + actual);
+            }
+
+            // Al final de todo avisamos que terminamos
+            tokens.Add(new Token(TipoToken.FinDeLinea));
+            return tokens;
+        }
+    }
 }
