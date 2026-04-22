daea5d08fb964d0767773a82f97cf31e47863192
Luciano Perondi <lucianoperondi90@gmail.com>
2026-04-22 18:28:10 -0300

    inicio compilador

--- practicos/63354 - Perondi, Luciano/tp2/Compilador.cs
+++ practicos/63354 - Perondi, Luciano/tp2/Compilador.cs
@@ -2,4 +2,32 @@ class Compilador {
     public static Nodo Parse(string expresion) {
         throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
     }
+
+    private string texto;
+    private int posicion;
+    private Token tokenActual;
+
+    private Compilador(string texto) {
+        this.texto = texto;
+        this.posicion = 0;
+        this.tokenActual = LeerToken();
+    }
 }
+
+
+enum TokenTipo {
+    numero,
+    variable,
+    suma,
+    resta,
+    multiplicacion,
+    division,
+    parentesisAbierto,
+    parentesisCerrado,
+    finExpresion
+}
+
+record Token(TipoToken Tipo, int Valor = 0);
+
+
+
