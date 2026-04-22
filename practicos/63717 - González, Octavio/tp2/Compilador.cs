9f56c3b16c4ef76b699e5bd36d4aac2bf832bf95
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-04-22 19:41:51 -0300

    3 commit tp2

--- practicos/63717 - González, Octavio/tp2/Compilador.cs
+++ practicos/63717 - González, Octavio/tp2/Compilador.cs
@@ -1,5 +1,32 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+using System;
+using System.Collections.Generic;
+
+namespace Calculadora
+{
+    // Aca pongo los tipos de cosas que podemos encontrar en la cuenta
+    public enum TipoToken
+    {
+        Numero,
+        Suma,
+        Resta,
+        Multiplicacion,
+        Division,
+        ParentesisIzquierdo,
+        ParentesisDerecho,
+        VariableX,
+        FinDeLinea // Para saber cuando se termina la formula
+    }
+
+    // Esta clase guarda que tipo es y si tiene algun valor (como los numeros)
+    public class Token
+    {
+        public TipoToken Tipo;
+        public string Valor;
+
+        public Token(TipoToken tipo, string valor = "")
+        {
+            Tipo = tipo;
+            Valor = valor;
+        }
     }
 }
