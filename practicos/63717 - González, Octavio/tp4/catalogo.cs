31f6357a9b6e303fcd652b43bf6f4c4dfc1bd684
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-05-31 17:12:41 -0300

    2- Nada relevante, solo muestra todos los productos, en vez de 1 solo

--- practicos/63717 - González, Octavio/tp4/catalogo.cs
+++ practicos/63717 - González, Octavio/tp4/catalogo.cs
@@ -7,43 +7,61 @@ using Terminal.Gui.Views;
 
 // ── Consulta inicial al servidor ──────────────────────────────────────────
 
-ProductoDto producto;
+// ProductoDto producto; //no terminada 
+// try {
+//     using var http = new HttpClient();
+//     int id = 0; 
+//     producto = await CargarProductoAsync(http, id);
+// } catch (HttpRequestException ex) {
+//     Console.Error.WriteLine($"No se pudo conectar con el servidor: {ex.Message}");
+//     Console.Error.WriteLine("Verificá que servidor.cs esté corriendo");
+//     return;
+// }
+
+ProductoDto[] productos;
 try {
     using var http = new HttpClient();
-    producto = await CargarProductoAsync(http);
-} catch (HttpRequestException ex) {
-    Console.Error.WriteLine($"No se pudo conectar con el servidor: {ex.Message}");
-    Console.Error.WriteLine("Verificá que servidor.cs esté corriendo en http://localhost:5050");
+    productos = await CargarProductosAsync(http);
+} catch (Exception ex) {
+    Console.Error.WriteLine($"{ex.Message}");
     return;
 }
 
 // ── Interfaz TUI ──────────────────────────────────────────────────────────
 
 using IApplication app = Application.Create().Init();
-using Window ventana = new () { Title = " Catalogo REST — Producto (ESC para salir) " };
+using Window ventana = new () { Title = " Catalogo de Productos (ESC para salir) " };
 
 var detalleProducto = new Label {
-    Text = $"""
-            # PRODUCTO 
-
-            - Id     : {producto.Id}
-            - Código : {producto.Codigo}
-            - Nombre : {producto.Nombre}
-            - Precio : ${producto.Precio,10:N2}
-            - Stock  :  {producto.Stock,10}
-            """,
+    Text = string.Join("\n\n", productos.Select(p => $"""
+            # PRODUCTOS
+            - Id     : {p.Id, 1}
+            - Código : {p.Codigo, 1}
+            - Nombre : {p.Nombre, 1}
+            - Precio : {p.Precio, 1}
+            - Stock  : {p.Stock, 1}
+            """)),
     X = 4, Y = 2,
-};
-
+};  
 ventana.Add(detalleProducto);
+
 
 app.Run(ventana);
 
-static async Task<ProductoDto> CargarProductoAsync (HttpClient http) {
-    const string url = "http://localhost:5050/producto";
-    return await http.GetFromJsonAsync<ProductoDto>(url) ?? throw new HttpRequestException("El servidor devolvió un producto vacío");
+static async Task<ProductoDto[]> CargarProductosAsync (HttpClient http) {
+    const string url = "http://localhost:3000/productos";
+    return await http.GetFromJsonAsync<ProductoDto[]>(url) ?? throw new HttpRequestException("No hay productos");
 }
 
+static async Task<ProductoDto> CargarProductoAsync (HttpClient http, int id) {
+    string url = $"http://localhost:3000/productos/{id}";
+    return await http.GetFromJsonAsync<ProductoDto>(url) ?? throw new HttpRequestException("No existe un producto con este ID");
+}
 // ── DTO ───────────────────────────────────────────────────────────────────
 
 record ProductoDto(int Id, string Codigo, string Nombre, decimal Precio, int Stock);
+// record MovimientoDTO(int Id, int Codigo, TipoMovimiento Tipo, int Cantidad,DateTime Fecha,
+// int ProductoId) 
+// {
+//     public Producto? Producto { get; set; }
+// }
