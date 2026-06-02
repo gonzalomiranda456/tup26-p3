557d006c700aa1098bc251cde1dbe517798794ea
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-06-02 17:08:32 -0300

    10 - funcion Refrescar Detalle, mejora color, mapeo de teclas

--- practicos/63717 - González, Octavio/tp4/catalogo.cs
+++ practicos/63717 - González, Octavio/tp4/catalogo.cs
@@ -11,6 +11,8 @@ using Terminal.Gui.Drawing;
 using Terminal.Gui.Configuration;
 using System.Collections.ObjectModel;
 using System.Xml.Serialization;
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 #pragma warning disable CS0618
@@ -24,18 +26,6 @@ try
 {
     using var http = new HttpClient();
     productos = await ObtenerProductos(http);
-}
-catch (Exception ex)
-{
-    Console.Error.WriteLine($"{ex.Message}");
-    return;
-}
-List<MovimientoDto> movimientos;
-try
-{
-    int id = 2;
-    using var http = new HttpClient();
-    movimientos = await ObtenerMovimientos(http, id);
 }
 catch (Exception ex)
 {
@@ -64,6 +54,7 @@ ConfigurationManager.Apply();
 
 Color fondo = new Color(30, 30, 46);
 
+
 var esquemaestro = new Scheme
 {
     Normal = new Terminal.Gui.Drawing.Attribute(Color.Green, fondo),
@@ -76,132 +67,217 @@ var esquedetalle = new Scheme
     Focus = new Terminal.Gui.Drawing.Attribute(Color.White, Color.Green),
     ReadOnly = new Terminal.Gui.Drawing.Attribute(Color.Green, Color.Black)
 };
-
-SchemeManager.AddScheme("Esquemaestro", esquemaestro);
-SchemeManager.AddScheme("esquedetalle", esquedetalle);
-
-using (IApplication app = Application.Create().Init())
+var esquemagui = new Scheme
 {
 
-    Window gui = new() { };
+    Normal = new Terminal.Gui.Drawing.Attribute(Color.Green, fondo)
+};
+SchemeManager.AddScheme("Esquemaestro", esquemaestro);
+SchemeManager.AddScheme("esquedetalle", esquedetalle);
+SchemeManager.AddScheme("gui", esquemagui);
 
-    //Menu
+using IApplication app = Application.Create().Init();
+using Window gui = new() { SchemeName = "gui" };
 
-    var menu = new MenuBar
+int salir = 0;
+
+//Para salir 
+gui.KeyDown += (sender, e) =>
+{
+    if (e.KeyCode == Key.Esc)
     {
-        Menus = [
-            new MenuBarItem("_Archivo", [
+        e.Handled = true;
+        salir = salir + 1;
+        if (salir == 2)
+        {
+            e.Handled = false;
+            app.RequestStop();
+        }
+    }
+};
+
+var dialogosalir = new Dialog
+{
+    Title = "Seguro desea salir?",
+    X = Pos.Center(),
+    Y = Pos.Center(),
+    SchemeName = "dialog"
+};
+
+//Menu
+
+var menu = new MenuBar
+{
+    Menus = [
+        new MenuBarItem("_Archivo", [
             new MenuItem("_Agregar", "", () => {}),
             new MenuItem("Salir", "", () => app.RequestStop())
         ]),
         new MenuBarItem("_Movimientos", [
             new MenuItem("_Compra", "", () => {}),
         ])
-        ],
-        SchemeName = "Esquemaestro"
+    ],
+    SchemeName = "Esquemaestro"
 
-    };
+};
 
-    var buscar = new Label
+var buscar = new Label
+{
+    Text = " Buscar:",
+    X = 3,
+    Y = 2,
+    SchemeName = "Esquemaestro"
+};
+
+var input = new TextField()
+{
+    X = Pos.Right(buscar),
+    Y = Pos.Top(buscar),
+    Width = 20,
+    SchemeName = "Esquemaestro"
+};
+
+//maestro
+
+var maestro = new FrameView
+{
+    Title = "Productos",
+    X = 0,
+    Y = 4,
+    Width = 25,
+    Height = Dim.Percent(90),
+    SchemeName = "Esquemaestro",
+    CanFocus = true
+
+};
+
+var panelmaestro = new ListView
+{
+    X = 0,
+    Y = 0,
+    Width = Dim.Fill(),
+    Height = Dim.Fill(1),
+    CanFocus = true
+};
+
+
+panelmaestro.SetSource(new ObservableCollection<string>(productos
+.Select(p => "ID " + p.Id + " - " + p.Nombre)
+.ToList()
+));
+
+maestro.Add(panelmaestro);
+
+//detalle
+
+var detalle = new FrameView
+{
+    Title = "Detalle",
+    X = Pos.Right(maestro),
+    Y = Pos.Top(maestro),
+    Width = Dim.Fill(),
+    Height = Dim.Percent(90),
+    SchemeName = "esquedetalle",
+    CanFocus = false
+};
+
+
+var listadetalles = new TextView
+{
+    X = 0,
+    Y = 0,
+    Width = Dim.Fill(),
+    Height = Dim.Fill(),
+    ReadOnly = true,
+    SchemeName = "esquedetalle",
+    WordWrap = true
+};
+detalle.Add(listadetalles, buscar);
+
+var teclasdisponibles = new Label
+{
+    Text = "teclas disponibles",
+    X = Pos.Right(detalle),
+    Y = Pos.Bottom(maestro),
+    Width = Dim.Fill(),
+    Height = Dim.Fill(),
+    SchemeName = "esquedetalle",
+};
+
+//Navegacion 
+panelmaestro.KeyDown += (sender, e) =>
+{
+    if (e.KeyCode == Key.CursorDown && panelmaestro.SelectedItem == panelmaestro.Source?.Count - 1) 
+    e.Handled = true;
+    if (e.KeyCode == Key.CursorUp && panelmaestro.SelectedItem == 0)
+    e.Handled = true;
+};
+
+input.KeyDown += (sender, e) =>
+{
+    if (e.KeyCode == Key.Enter || e.KeyCode == Key.Esc)
     {
-        Text = " Buscar:",
-        X = 3,
-        Y = 2,
-        SchemeName = "Esquemaestro"
-    };
+        panelmaestro.SetFocus();
+        e.Handled = true;
+    }
+};
 
-    var input = new TextField()
+
+//Añadir las views
+gui.Add(menu, maestro, detalle, buscar, input, teclasdisponibles);
+panelmaestro.ValueChanged += async (sender, e) => await Refrescardetalle(e.NewValue);
+
+app.Run(gui);
+
+
+// Funciones ----------------------------------------------------
+
+async Task Refrescardetalle(int? indice)
+{
+
+    if (indice is null || indice < 0 || indice > productos.Count)
     {
-        X = Pos.Right(buscar),
-        Y = Pos.Top(buscar),
-        Width = 20,
-        SchemeName = "Esquemaestro"
-    };
+        detalle.Text = "Nada seleccionado";
+        return;
+    }
+    var prodseleccionado = productos[indice.Value];
 
-    //maestro
-
-    var maestro = new FrameView
+    List<MovimientoDto> movimientosDelProducto = new();
+    try
     {
-        Title = "Productos",
-        X = 0,
-        Y = 4,
-        Width = 25,
-        Height = Dim.Fill(1),
-        SchemeName = "Esquemaestro",
-        CanFocus = true
-    };
-
-    var panelmaestro = new ListView
+        using var http = new HttpClient();
+        string url = $"http://localhost:3000/productos/{prodseleccionado.Id}/movimientos";
+        movimientosDelProducto = await http.GetFromJsonAsync<List<MovimientoDto>>(url) ?? new();
+    }
+    catch (Exception ex)
     {
-        X = 0,
-        Y = 0,
-        Width = Dim.Fill(),
-        Height = Dim.Fill(1),
-        CanFocus = true
-    };
+        detalle.Text = $"No se puedo obtener movimientos - Error : {ex.Message}";
+        return;
+    }
 
-    panelmaestro.SetSource(new ObservableCollection<string>(productos
-    .Select(p => "ID " + p.Id + " - " + p.Nombre)
-    .ToList()
-    ));
-
-    maestro.Add(panelmaestro);
-
-    //detalle
-
-    var detalle = new FrameView
-    {
-        Title = "Detalle",
-        X = Pos.Right(maestro),
-        Y = Pos.Top(maestro),
-        Width = Dim.Fill(),
-        Height = Dim.Fill(),
-        SchemeName = "esquedetalle"
-    };
-
-
-    var detalles = new TextView
-    {
-        Text = string.Join("\n\n", movimientos.Select(m => $"""
-    
+    listadetalles.Text = string.Join("\n\n", movimientosDelProducto.Select(m =>
+    $"""    
         ID Movimiento: {m.Id}
 
         Tipo:          {m.Tipo switch
-                        {
-                            1 => "Compra",
-                            2 => "Venta",
-                            3 => "Ajuste",
-                            _ => "No asignado"
-                        }}
+    {
+        1 => "Compra",
+        2 => "Venta",
+        3 => "Ajuste",
+        _ => "No asignado"
+    }}
 
         Descripcion:   {m.ProductoId}
 
         Cantidad:      {m.Cantidad}
 
-        Stock :        {producto.Stock} unidades
+        Stock :         unidades
 
         Fecha:         {m.Fecha}
-
-    """)),
-        X = 0,
-        Y = 0,
-        Width = Dim.Fill(),
-        Height = Dim.Fill(),
-        ReadOnly = true,
-        SchemeName = "esquedetalle",
-        WordWrap=true
-    };
-    detalle.Add(detalles, buscar);
-
-
-    //Añadir las views
-    gui.Add(menu, maestro, detalle, buscar, input);
-    gui.SchemeName = "Dialog";
-
-    app.Run(gui);
+        
+    """));
 }
 
-// Funciones ----------------------------------------------------
 
 async void Refrescar()
 {
