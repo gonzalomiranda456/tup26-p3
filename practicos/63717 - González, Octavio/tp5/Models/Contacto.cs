417c697ac88498f52b8e639328b348464654c38d
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-06-10 07:46:55 -0300

    3 - correcion en contacto, obligatoriedad de campos

--- practicos/63717 - González, Octavio/tp5/Models/Contacto.cs
+++ practicos/63717 - González, Octavio/tp5/Models/Contacto.cs
@@ -1,16 +1,30 @@
 namespace tp5.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
 public class Contacto
+
 {
+    [Required]
     public int Id { get; set; }
+    [Required]
     public string Nombre { get; set; } = "";
+    [Required]
     public string Apellido { get; set; } = "";
+    [Required]
+    [MaxLength(20)]
     public string Telefono { get; set; } = "";
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = "";
+
     public string Empresa { get; set; } = "";
+
     public string Cargo { get; set; } = "";
+
     public string Direccion { get; set; } = "";
+  
     public DateOnly? FechaNacimiento { get; set; }
     public string Notas { get; set; } = "";
 }
